e0b0b6cdef96146641ae7feae81e528d9da86b30
Kristian Brimble <brimble2010@gmail.com>
2016-08-15 09:45:02 +0100

    AutoFac builder extension for AutoConfig

---
+++ KBrimble.AutoConfig.AutoFac/AutoConfigAutoFacExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Autofac.Builder;
+
+namespace KBrimble.AutoConfig.AutoFac
+{
+    public static class AutoConfigAutoFacExtensions
+    {
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterConfig<T>(this ContainerBuilder builder, string sectionName = null) where T : class
+        {
+            return builder.RegisterInstance(Nerdle.AutoConfig.AutoConfig.Map<T>()).As<T>();
+        }
+    }
+}
